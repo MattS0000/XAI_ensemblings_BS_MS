d60e9f3da83cfc102cec1478f98cae8e706260ac
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-11-10 18:22:20 +0100

    add simple avg, min, max ensembling with unit tests.

--- EnsembleXAI/Ensemble.py
+++ EnsembleXAI/Ensemble.py
@@ -1,13 +1,40 @@
-from torch import Tensor
-from typing import TypeVar, Tuple, List, Callable
+import torch
 
-TensorOrTupleOfTensorsGeneric = TypeVar(
-    "TensorOrTupleOfTensorsGeneric", Tensor, Tuple[Tensor, ...]
-)
+from copy import deepcopy
+from torch import Tensor, stack
+from typing import TypeVar, Tuple, List, Callable, Union
+
+TensorOrTupleOfTensorsGeneric = TypeVar("TensorOrTupleOfTensorsGeneric", Tensor, Tuple[Tensor, ...])
 
 
-def aggregate(inputs: TensorOrTupleOfTensorsGeneric, aggregating_func: str) -> Tensor:
-    pass
+def aggregate(inputs: TensorOrTupleOfTensorsGeneric, aggregating_func: Union[str, Callable]) -> Tensor:
+    # input tensor dims: observations x explanations x single explanation
+    if isinstance(aggregating_func, str):
+        assert aggregating_func in ['avg', 'min', 'max']
+
+    parsed_inputs = deepcopy(inputs)
+    if isinstance(inputs, tuple):
+        if inputs[0].dim() <= 3:
+            # multiple observations with explanations as tensor
+            parsed_inputs = stack(inputs)
+    if parsed_inputs.dim() == 3:
+        # single observation with multiple explanations
+        parsed_inputs = parsed_inputs[None, :]
+
+    input_size = parsed_inputs.size()
+    n_explanations = input_size[1]
+    new_size = (input_size[0], 1, input_size[2], input_size[3])
+
+    if aggregating_func == 'avg':
+        output = torch.squeeze(1 / n_explanations * parsed_inputs.sum_to_size(new_size), dim=1)
+
+    if aggregating_func == 'max':
+        output = parsed_inputs.amax(1)
+
+    if aggregating_func == 'min':
+        output = parsed_inputs.amin(1)
+
+    return output
 
 
 def ensemble(inputs: TensorOrTupleOfTensorsGeneric, metrics: List[Callable], weights: List[float]) -> Tensor:
--- EnsembleXAI/tests/test_Ensemble.py
+++ EnsembleXAI/tests/test_Ensemble.py
@@ -1,8 +1,60 @@
-from unittest import TestCase
+import torch as t
+
 from EnsembleXAI import Ensemble
+from unittest import TestCase
 
 
 class TestAggregate(TestCase):
-    def test_aggregate(self):
-        Ensemble.aggregate()
-        self.fail()
+    exp1 = t.ones([2, 2])
+    exp3 = 3 * t.ones([2, 2])
+    obs1_tensor = t.stack((exp1, exp3))
+    exp0 = t.zeros([2, 2])
+    obs2_tensor = t.stack([exp0, exp3])
+
+    mult_obs_tensor = t.stack([obs1_tensor, obs2_tensor])
+
+    def test_one_obs_avg(self):
+        # tuple input
+        ensembled = Ensemble.aggregate((self.exp1, self.exp3), 'avg')
+        self.assertIsInstance(ensembled, t.Tensor)
+
+        expected = 2 * t.ones([1, 2, 2])
+        self.assertTrue(t.equal(ensembled, expected))
+        # tensor input
+        ensembled = Ensemble.aggregate(self.obs1_tensor, 'avg')
+        self.assertTrue(t.equal(ensembled, expected))
+
+    def test_one_obs_max(self):
+        ensembled = Ensemble.aggregate((self.exp1, self.exp3), 'max')
+        self.assertIsInstance(ensembled, t.Tensor)
+
+        expected = 3 * t.ones([1, 2, 2])
+        self.assertTrue(t.equal(ensembled, expected))
+        # tensor input
+        ensembled = Ensemble.aggregate(self.obs1_tensor, 'max')
+        self.assertTrue(t.equal(ensembled, expected))
+
+    def test_one_obs_min(self):
+        ensembled = Ensemble.aggregate((self.exp1, self.exp3), 'min')
+        self.assertIsInstance(ensembled, t.Tensor)
+
+        expected = t.ones([1, 2, 2])
+        self.assertTrue(t.equal(ensembled, expected))
+        # tensor input
+        ensembled = Ensemble.aggregate(self.obs1_tensor, 'min')
+        self.assertTrue(t.equal(ensembled, expected))
+
+    def test_multi_obs_avg(self):
+        # tuple input
+        ensembled = Ensemble.aggregate((self.obs1_tensor, self.obs2_tensor), 'avg')
+        self.assertIsInstance(ensembled, t.Tensor)
+        expected = t.stack([2 * t.ones([2, 2]), 1.5 * t.ones([2, 2])])
+        self.assertTrue(t.equal(ensembled, expected))
+
+        # tensor input
+        ensembled = Ensemble.aggregate(self.mult_obs_tensor, 'avg')
+        self.assertTrue(t.equal(ensembled, expected))
+
+    def test_illegal_args(self):
+        with self.assertRaises(AssertionError):
+            Ensemble.aggregate(self.obs1_tensor, 'asdf')
