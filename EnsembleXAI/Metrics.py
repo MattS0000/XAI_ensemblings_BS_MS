dbf8330d717f59149565d88f2b7f86d6134cfaaa
MattS0000 <mati201420@gmail.com>
2022-11-17 13:53:41 +0100

    Refactor metrics to accept 4d torch tensors

--- EnsembleXAI/Metrics.py
+++ EnsembleXAI/Metrics.py
@@ -1,15 +1,23 @@
 from captum.attr import Attribution
+from torch import Tensor
 from torch.utils.data import Dataset
-from typing import Callable, Union, List
+from typing import Callable, Union, List, Tuple
 import itertools
 import torch
 
 
 def replace_mask(image: torch.Tensor, mask: torch.Tensor, value: Union[int, float]) -> torch.Tensor:
     temp_image = torch.clone(image)
-    reshaped_mask = mask.repeat(3, 1, 1)
+    reshaped_mask = mask.unsqueeze(dim=1).repeat(1, 3, 1, 1)
     temp_image[reshaped_mask] = value
     return temp_image
+
+
+def explanations_to_list(explanations: torch.Tensor, depth: int) -> List[torch.Tensor]:
+    tensor_list = [x.squeeze() for x in torch.tensor_split(explanations, explanations.shape[0], dim=0)]
+    for i in range(depth-1):
+        tensor_list = [y.squeeze() for x in tensor_list for y in torch.tensor_split(x, x.shape[0], dim=0)]
+    return tensor_list
 
 
 def _matrix_norm_2(exp1: torch.Tensor, exp2: torch.Tensor) -> torch.Tensor:
@@ -39,7 +47,7 @@ def _union(
     )
 
 
-def consistency(explanations: List[torch.Tensor]) -> torch.Tensor:
+def consistency(explanations: torch.Tensor) -> torch.Tensor:
     """
     Opis: Mierzy jak bardzo wyjaśnienia różnych modeli uczenia maszynowego są do siebie podobne.
     Argumenty wejściowe: Lista<torch.Tensor> (lista wyjaśnień które chcemy ze sobą porównać)
@@ -48,9 +56,10 @@ def consistency(explanations: List[torch.Tensor]) -> torch.Tensor:
     C(phi,...) = [max_{a,b}(||phi_{j}^{e->m_a} - phi_{j}^{e->m_b}||_2) + 1]^{-1}, phi_j-wyjasnienie j tego zdjęcia lub
     [max_{a,b}(||phi_{j}^{e_a->m} - phi_{j}^{e_b->m}||_2) + 1]^{-1}
     """
+    explanations_list = explanations_to_list(explanations, depth=2)
     diffs = [
         _matrix_norm_2(exp1, exp2)
-        for exp1, exp2 in itertools.combinations(explanations, 2)
+        for exp1, exp2 in itertools.combinations(explanations_list, 2)
     ]
     return 1 / max(diffs) + 1
 
@@ -69,11 +78,23 @@ def stability(
     pass
 
 
-def decision_impact_ratio(
-    dataset: Dataset,
+def _impact_ratio_helper(
+    images_tensor: torch.Tensor,
     predictor: Callable[..., torch.Tensor],
-    explanator: Attribution,
-    baseline: torch.Tensor,
+    explanations: torch.Tensor,
+    baseline: int,
+) -> tuple[Tensor, Tensor]:
+    probabilities_original = predictor(images_tensor)
+    modified_images = replace_mask(images_tensor, explanations, baseline)
+    probabilities_modified = predictor(modified_images)
+    return probabilities_original, probabilities_modified
+
+
+def decision_impact_ratio(
+    images_tensor: torch.Tensor,
+    predictor: Callable[..., torch.Tensor],
+    explanations: torch.Tensor,
+    baseline: int,
 ) -> torch.Tensor:
     """
     Czy chcemy tylko na tescie to robic?
@@ -84,14 +105,20 @@ def decision_impact_ratio(
     :return:
     DIR = Suma po i (1 jeżeli D(x_i)=/=D(x_i-c_i) else 0)/N, D to klasyfikacja, c_i obszar krytyczny
     """
-    pass
+    n = images_tensor.shape[0]
+    #predictor returns probabilities in a tensor format
+    probs_original, probs_modified = _impact_ratio_helper(images_tensor, predictor, explanations, baseline)
+    preds_original = torch.max(probs_original)
+    preds_modified = torch.max(probs_modified)
+    value = torch.sum(preds_original != preds_modified)/n
+    return value
 
 
 def confidence_impact_ratio(
-    dataset: Dataset,
-    probs: Callable[..., torch.Tensor],
-    explanator: Attribution,
-    baseline: torch.Tensor,
+    images_tensor: torch.Tensor,
+    predictor: Callable[..., torch.Tensor],
+    explanations: torch.Tensor,
+    baseline: int = 0,
 ) -> torch.Tensor:
     """
     Opis: Średni spadek estymowanego prawdopodobieństwa klasyfikacji po zasłonięciu obszaru wrażliwości.
@@ -100,11 +127,13 @@ def confidence_impact_ratio(
     :return:
     CIR = Suma po i max(C(x_i)-C(x_i-c_i), 0)/N , C to probabilities, c_i obszar krytyczny
     """
-    pass
+    probs_original, probs_modified = _impact_ratio_helper(images_tensor, predictor, explanations, baseline)
+    value = torch.max(probs_original - probs_modified)/images_tensor.shape[0]
+    return value
 
 
 def accordance_recall(
-    mask: torch.Tensor, explanation: torch.Tensor, threshold: float = 0.5
+    masks: torch.Tensor, explanations: torch.Tensor, threshold: float = 0.5
 ) -> torch.Tensor:
     """
         Opis: Mierzy jaką część maski wykryło wyjaśnienie.
@@ -120,12 +149,14 @@ def accordance_recall(
         recall = sum_i(recall_i)/N
     """
     # maska logiczna, jest czy nie jest w masce
-    overlaping_area = _intersection(explanation, mask, threshold1=threshold)
-    return torch.sum(overlaping_area) / torch.sum(torch.abs(mask) != 0)
+    # dla jednego wyjasnienia, wiec wymiar explanations.shape = (n, 1, width, height) mask = (n, width, height)
+    squeezed_expl = explanations.squeeze(dim=1)
+    overlaping_area = _intersection(squeezed_expl, masks, threshold1=threshold)
+    return torch.sum(overlaping_area, dim=(-2, -1)) / torch.sum(torch.abs(masks) != 0, dim=(-2, -1))
 
 
 def accordance_precision(
-    mask: torch.Tensor, explanation: torch.Tensor, threshold: float = 0.5
+    masks: torch.Tensor, explanations: torch.Tensor, threshold: float = 0.5
 ) -> torch.Tensor:
     """
         Opis: mierzy jaką część wyjaśnienia stanowiła maska.
@@ -141,30 +172,13 @@ def accordance_precision(
         precision = sum_i (precision_i)/N
     """
     # maska logiczna, jest czy nie jest w masce
-    overlaping_area = _intersection(explanation, mask, threshold1=threshold)
-    return torch.sum(overlaping_area) / torch.sum(torch.abs(explanation) > threshold)
-
-
-def set_accordance_recall(masks, explanations, threshold=0.5):
-    # tensor with a scalar value is expected as a result of accordance_recall
-    recalls = [
-        accordance_recall(mask, expl, threshold=threshold).item()
-        for mask, expl in zip(masks, explanations)
-    ]
-    return recalls
-
-
-def set_accordance_precision(masks, explanations, threshold=0.5):
-    # tensor with a scalar value is expected as a result of accordance_precision
-    precs = [
-        accordance_precision(mask, expl, threshold=threshold).item()
-        for mask, expl in zip(masks, explanations)
-    ]
-    return precs
+    squeezed_expl = explanations.squeeze(dim=1)
+    overlaping_area = _intersection(squeezed_expl, masks, threshold1=threshold)
+    return torch.sum(overlaping_area, dim=(-2, -1)) / torch.sum(torch.abs(squeezed_expl) > threshold, dim=(-2, -1))
 
 
 def F1_score(
-    masks: List[torch.Tensor], explanations: List[torch.Tensor], threshold: float = 0.5
+    masks: torch.Tensor, explanations: torch.Tensor, threshold: float = 0.5
 ) -> torch.Tensor:
     """
         Opis: Średnia harmoniczna Accordance recall i Accordance precision.
@@ -178,17 +192,14 @@ def F1_score(
     is identified by the interpretation method
         Accordance F1 = 1/N * sum_i (2*(recall_i + precision_i)/(recall_i*precision_i))
     """
-    acc_recall = set_accordance_recall(masks, explanations, threshold=threshold)
-    acc_prec = set_accordance_precision(masks, explanations, threshold=threshold)
-    values = [
-        2 * (recall * prec) / (recall + prec)
-        for (recall, prec) in zip(acc_recall, acc_prec)
-    ]
-    return sum(values) / len(values)
+    acc_recall = accordance_recall(masks, explanations, threshold=threshold)
+    acc_prec = accordance_precision(masks, explanations, threshold=threshold)
+    values = 2 * (acc_recall * acc_prec) / (acc_recall + acc_prec)
+    return torch.sum(values) / values.shape[0]
 
 
 def intersection_over_union(
-    masks: List[torch.Tensor], explanations: List[torch.Tensor], threshold: float = 0.5
+    masks: torch.Tensor, explanations: torch.Tensor, threshold: float = 0.5
 ) -> torch.Tensor:
     """
         Opis: Pole iloczynu maski i wyjaśnienia podzielone przez pole sumy maski i wyjaśnienia.
@@ -202,12 +213,11 @@ def intersection_over_union(
     is identified by the interpretation method
         IOU=1/N * sum_i(S(x_i) cz. wspolna F(x_i)/S(x_i) suma F(x_i))
     """
-    values = [
-        torch.sum(_intersection(expl, mask, threshold1=threshold))
-        / torch.sum(_union(expl, mask, threshold1=threshold))
-        for (expl, mask) in zip(explanations, masks)
-    ]
-    return sum(values) / len(values)
+    squeezed_expl = explanations.squeeze(dim=1)
+    values = torch.sum(_intersection(squeezed_expl, masks, threshold1=threshold), dim=(-2, -1)) \
+             / torch.sum(_union(squeezed_expl, masks, threshold1=threshold), dim=(-2, -1))
+
+    return torch.sum(values) / values.shape[0]
 
 
 def ensemble_score(
